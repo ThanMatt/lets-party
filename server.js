349b74eb93abac43b8c7fc50e05bd1ec46fdc852
Thanmatt <aethanilagan@gmail.com>
2022-12-11 17:43:46 +0800

    Added encryption

--- server.js
+++ server.js
@@ -1,51 +1,80 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io')(server, { cors: { origin: '*' } });
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const crypto = require('crypto')
+const io = require('socket.io')(server, { cors: { origin: '*' } })
 
-const port = process.env.PORT || 4000;
+require('dotenv').config()
+
+const ALGORITHM = 'aes-128-cbc'
+
+const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
-	res.send('<h1>Lets Party Server</h1>');
-});
+  res.send('<h1>Lets Party Server</h1>')
+})
+
+app.get('/watch/:id', (req, res) => {
+  const decipher = crypto.createDecipheriv(ALGORITHM, process.env.SECURITY_KEY, process.env.INIT_VECTOR)
+
+  let decryptedData = decipher.update(req.params.id, 'hex', 'utf-8')
+
+  decryptedData += decipher.final('utf8')
+  return res.send({
+    decrypted: decryptedData,
+    query: req.query
+  })
+})
 
 io.on('connection', (socket) => {
-	socket.emit('whoami', { id: socket.id });
-	// join to the room
-	socket.on('joinmetothisroom', ({ roomId, name }) => {
-		socket.join(roomId);
-		socket.emit('joinmetothisroomsuccess', `${roomId} `);
-		io.to(roomId).emit('someonejoined', name);
-	});
+  socket.emit('whoami', { id: socket.id })
+  // join to the room
+  socket.on('joinmetothisroom', ({ roomId, name, urlToWatch }) => {
+    console.log(urlToWatch)
+    const cipher = crypto.createCipheriv(ALGORITHM, process.env.SECURITY_KEY, process.env.INIT_VECTOR)
 
-	// tell everyone who are here in the room
-	socket.on('tell_everyone_who_joined', ({ allusers, roomId }) => {
-		io.to(roomId).emit('who_joined', allusers);
-	});
+    let encryptedData = cipher.update(urlToWatch, 'utf-8', 'hex')
 
-	// check connection
-	socket.on('msg', ({ data, roomId }) => {
-		io.to(roomId).emit('msg', data);
-	});
+    encryptedData += cipher.final('hex')
 
-	socket.on('sendMessage', ({ roomId, ...args }) => {
-		console.log('Room ID: ', roomId)
-		console.log('Message payload: ', args)
-		io.to(roomId).emit('newMessage', args)
-	})
+    const inviteUrl = `http://localhost:4000/watch/${encryptedData}?room_code=${roomId}`
 
-	// get video state
-	socket.on('videoStates', ({ videoState, roomId }) => {
-		io.to(roomId).emit('videoStates', videoState);
-	});
+    socket.join(roomId)
+    socket.emit('joinmetothisroomsuccess', {
+      roomId,
+      inviteUrl
+    })
+    io.to(roomId).emit('someonejoined', name)
+  })
 
-	// disconnect
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
-	});
-});
+  // tell everyone who are here in the room
+  socket.on('tell_everyone_who_joined', ({ allusers, roomId }) => {
+    io.to(roomId).emit('who_joined', allusers)
+  })
+
+  // check connection
+  socket.on('msg', ({ data, roomId }) => {
+    io.to(roomId).emit('msg', data)
+  })
+
+  socket.on('sendMessage', ({ roomId, ...args }) => {
+    console.log('Room ID: ', roomId)
+    console.log('Message payload: ', args)
+    io.to(roomId).emit('newMessage', args)
+  })
+
+  // get video state
+  socket.on('videoStates', ({ videoState, roomId }) => {
+    io.to(roomId).emit('videoStates', videoState)
+  })
+
+  // disconnect
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 server.listen(port, () => {
-	console.log(`listening on ${port}`);
-});
+  console.log(`listening on ${port}`)
+})
